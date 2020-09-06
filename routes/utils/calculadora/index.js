794a2ce4ded718b880edc9ac3fb7cc686f69be09
luistapls <llopezampiez7@gmail.com>
2020-09-06 15:17:55 -0430

    pokeFix

--- routes/utils/calculadora/index.js
+++ routes/utils/calculadora/index.js
@@ -1,31 +1,23 @@
-/* eslint-disable*/
-
+/* eslint-disable */
 // Import modules
 const express = require('express');
+const { operaciones } = require('../../../services/calculadora/math');
+const { powerfulPoke } = require('../../../services/calculadora/powerfulPoke');
 
-// middleware
+// Middlewares
 const {
   isNumeroCalculadora,
 } = require('../../../utils/middleware/calculadora');
 
-// Service Calculadora
-const { operaciones } = require('../../../services/calculadora/math');
-const { powerfulPoke } = require('../../../services/calculadora/powerfulPoke');
-
+// Calculadora route
 const calculadora = (app) => {
   const router = express.Router();
   app.use('/api/calculadora', router);
 
-  // Pokemon
-  router.get('/pokemon/:poke1/:poke2', async (req, res) => {
-    const { poke1, poke2 } = req.params;
-    const resultsPoke = await powerfulPoke(poke1, poke2);
-    resultsPoke.status === 404
-      ? res.status(404).json(resultsPoke)
-      : res.status(200).json(resultsPoke);
+  router.get('/pokemon', (req, res) => {
+    res.json(powerfulPoke(app.get('sendPoke')));
   });
 
-  // Calculadora
   router.get(
     '/:value1/:operator/:value2',
     isNumeroCalculadora(),
